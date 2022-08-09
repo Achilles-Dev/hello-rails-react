22ee44f1b694e8b55263c9f753f8aeac60d64306
Achilles-Dev <solohag92@gmail.com>
2022-08-09 07:32:11 +0000

    Add static view as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/api/greeting', to: 'api_endpoints#random_greeting'
+
+  root 'static#index'
 end
